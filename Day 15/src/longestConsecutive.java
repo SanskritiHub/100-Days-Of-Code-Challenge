2671a2a78b15825ca13a05f72648a4b53a8efc57
sanskriti <sanskritisandhyagupta@gmail.com>
2023-04-20 22:45:56 +0530

    Day 15 Code

---
+++ Day 15/src/longestConsecutive.java
@@ -0,0 +1,33 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class longestConsecutive {
+	
+	public static int longestConsecutive(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int i : nums){
+             set.add(i);
+        }
+        int ans = 0;
+        for(int i=0; i<nums.length; i++){
+            if(!set.contains(nums[i]-1)){
+                int j = nums[i] + 1;
+                while(set.contains(j)){
+                    j++;
+                }
+                ans = Math.max(ans, j - nums[i]);
+            }
+        }
+        return ans;
+    }
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int nums[] = {4,1,7,2,3,8};
+		System.out.println(longestConsecutive(nums));
+
+	}
+
+}
