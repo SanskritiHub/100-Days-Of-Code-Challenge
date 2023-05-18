3aec53b8c50a6cb71366baba8d062d8cacb1da1f
sanskriti <sanskritisandhyagupta@gmail.com>
2023-05-18 22:51:23 +0530

    Day 42 Code -reverse LL

---
+++ Day 42/src/ListNode.java
@@ -0,0 +1,28 @@
+public class ListNode {
+ 	  int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+ 
+class reverseLL {
+    public ListNode reverseList(ListNode head){
+
+        if(head==null){
+            return head;
+        }
+
+        ListNode curr = head;
+        ListNode prev = null;
+
+        while(curr!=null){
+            ListNode temp = curr.next;
+            curr.next= prev;
+            prev = curr;
+            curr = temp;
+        }   
+        return prev;
+    }
+
+}
