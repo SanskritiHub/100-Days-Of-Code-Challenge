c34823ce2e713cf3e1fc06327dfb8d342af1b34a
sanskriti <sanskritisandhyagupta@gmail.com>
2023-05-21 22:20:08 +0530

    Day 45 Code - invert BT and max depth of BT

---
+++ Day 45/src/invertBT.java
@@ -0,0 +1,23 @@
+public class invertBT {
+	
+	static class TreeNode {
+	      int val;
+	      TreeNode left;
+	      TreeNode right;
+	      TreeNode() {}
+	      TreeNode(int val) { this.val = val; }
+	      TreeNode(int val, TreeNode left, TreeNode right) {
+	          this.val = val;
+	          this.left = left;
+	          this.right = right;
+	      }
+	  }
+	
+    public TreeNode invertTree(TreeNode root) {
+        if(root==null) return null;
+        TreeNode node = new TreeNode(root.val);
+        node.right = invertTree(root.left);
+        node.left = invertTree(root.right);
+        return node;
+    }
+}
