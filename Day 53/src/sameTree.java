a05f2819a1fdf58acf6f8aab31c827abe406e657
sanskriti <sanskritisandhyagupta@gmail.com>
2023-05-30 20:07:32 +0530

    Day 53 Code - same tree

---
+++ Day 53/src/sameTree.java
@@ -0,0 +1,39 @@
+
+public class sameTree {
+	
+	public class TreeNode {
+	      int val;
+	      TreeNode left;
+	      TreeNode right;
+	      TreeNode() {}
+	      TreeNode(int val) { this.val = val; }
+	      TreeNode(int val, TreeNode left, TreeNode right) {
+	          this.val = val;
+	          this.left = left;
+	          this.right = right;
+	      }
+	  }
+	
+	public boolean isSameTree(TreeNode p, TreeNode q) {
+        return dfs(p, q);
+    }
+
+    private boolean dfs(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null) {
+            return false;
+        }
+
+        if (p.val != q.val) return false;
+
+        boolean left = dfs(p.left, q.left);
+        boolean right = dfs(p.right, q.right);
+
+        return left && right;
+    }
+}
+
+
