73432cc2174124cfb4992bb89b6cbc41d7672dfb
sanskriti <sanskritisandhyagupta@gmail.com>
2023-06-10 23:34:49 +0530

    Day 61 Code - max area of island

---
+++ Day 61/src/maxIsland.java
@@ -0,0 +1,46 @@
+
+public class maxIsland {
+	
+	public static int maxAreaOfIsland(int[][] grid) {
+        int max = 0;
+        for(int i=0; i<grid.length; i++){
+            for(int j=0; j<grid[i].length; j++){
+                if(grid[i][j] == 1){
+                    max = Math.max(max, dfs(grid, i, j));
+                }
+            }
+        }
+        return max;
+    }
+
+    public static int dfs(int[][] grid, int i, int j){
+        if(i<0 || i>=grid.length || j<0 || j>=grid[i].length || grid[i][j]== 0) {
+            return 0;
+        }
+
+        grid[i][j] = 0;
+        int count = 1; //present position
+        //neighbor
+        count += dfs(grid, i+1, j);
+        count += dfs(grid, i-1, j);
+        count += dfs(grid, i, j+1);
+        count += dfs(grid, i, j-1);
+        return count;
+    
+    }
+
+
+	public static void main(String args[]) {
+		int[][] grid = {{0,0,1,0,0,0,0,1,0,0,0,0,0},
+						{0,0,0,0,0,0,0,1,1,1,0,0,0},
+						{0,1,1,0,1,0,0,0,0,0,0,0,0},
+						{0,1,0,0,1,1,0,0,1,0,1,0,0},
+						{0,1,0,0,1,1,0,0,1,1,1,0,0},
+						{0,0,0,0,0,0,0,0,0,0,1,0,0},
+						{0,0,0,0,0,0,0,1,1,1,0,0,0},
+						{0,0,0,0,0,0,0,1,1,0,0,0,0}};
+		
+		System.out.println(maxAreaOfIsland(grid));
+	}
+
+}
