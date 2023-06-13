4fb8a85e589a21244b050ab566bb410282281ace
sanskriti <sanskritisandhyagupta@gmail.com>
2023-06-13 21:26:50 +0530

    Day 65 Code - lowest common ancestor

---
+++ Day 65/src/lowestCommonAncestor.java
@@ -0,0 +1,24 @@
+
+public class lowestCommonAncestor {
+	
+	 public class TreeNode {
+	      int val;
+	      TreeNode left;
+	      TreeNode right;
+	      TreeNode(int x) { val = x; }
+	  }
+	 
+
+	   public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+	        if(root == null) return null;
+	        if(root == p || root == q) return root;
+
+	        TreeNode left = lowestCommonAncestor(root.left, p , q);
+	        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+	        if(left == null) return right;
+	        if(right == null) return left;
+
+	        return root;
+	    }
+	}
