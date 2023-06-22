babd419866fdf5dd937eb7e7b959c7a868c18955
sanskriti <sanskritisandhyagupta@gmail.com>
2023-06-22 20:51:52 +0530

    Day 75 code - Find the Highest Altitude

---
+++ Day 75/src/highestAltitude.java
@@ -0,0 +1,22 @@
+
+public class highestAltitude {
+	
+	public static int largestAltitude(int[] gain) {
+        int maxAltitude = 0;
+        int currentAltitude = 0;
+        
+        for (int i = 0; i < gain.length; i++) {
+            currentAltitude += gain[i];
+            maxAltitude = Math.max(maxAltitude, currentAltitude);
+        }
+        
+        return maxAltitude;
+    }
+
+
+	public static void main(String[] args) {
+		int[] gain = {-5,1,5,0,-7};
+		System.out.println(largestAltitude(gain));
+	}
+
+}
