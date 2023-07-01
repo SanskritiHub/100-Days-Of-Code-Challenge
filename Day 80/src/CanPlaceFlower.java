c6f783cf72f8353b59eb98c9b629e833d30799ea
sanskriti <sanskritisandhyagupta@gmail.com>
2023-07-01 22:03:15 +0530

    Day 80 code - can place flower

---
+++ Day 80/src/CanPlaceFlower.java
@@ -0,0 +1,21 @@
+
+public class CanPlaceFlower {
+	
+	public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        if (n == 0) {
+            return true;
+        }
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i-1] == 0) && (i == flowerbed.length-1 || flowerbed[i+1] == 0)) {
+                flowerbed[i] = 1;
+                n--;
+                if (n == 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+
+
