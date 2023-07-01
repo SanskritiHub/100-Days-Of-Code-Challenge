360e404dec8ede67f8ae3baccc272899eba28a5d
sanskriti <sanskritisandhyagupta@gmail.com>
2023-07-01 22:06:37 +0530

    Day 86 code - maximum subarray

---
+++ Day 86/src/MaximumSubarray.java
@@ -0,0 +1,24 @@
+
+public class MaximumSubarray {
+
+	public int maxSubArray(int[] nums) {
+        int maxSum = Integer.MIN_VALUE;
+        int currentSum = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            currentSum += nums[i];
+            
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+            }
+            
+            if (currentSum < 0) {
+                currentSum = 0;
+            }
+        }
+        
+        return maxSum;
+    }
+}
+
+
