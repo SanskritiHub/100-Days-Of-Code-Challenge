ef597107b8cdbb24e1528296706e3dc64625b656
sanskriti <sanskritisandhyagupta@gmail.com>
2023-07-01 22:04:15 +0530

    Day 82 code - add binary

---
+++ Day 82/src/addBinary.java
@@ -0,0 +1,34 @@
+public class addBinary {
+
+	public String addbinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        int carry = 0;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        
+        while (i >= 0 || j >= 0) 
+        {
+            int sum = carry;
+            if (i >= 0) 
+            {
+                sum += a.charAt(i) - '0';
+                i--;
+            }
+            if (j >= 0) 
+            {
+                sum += b.charAt(j) - '0';
+                j--;
+            }
+            sb.append(sum % 2);
+            carry = sum / 2;
+        }
+        
+        if (carry > 0) 
+        {
+            sb.append(carry);
+        }
+        
+        return sb.reverse().toString();
+
+	}
+}
