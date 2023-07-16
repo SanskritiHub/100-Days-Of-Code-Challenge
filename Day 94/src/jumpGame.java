1644ed4585ebf945bb2167469c392725a44e410c
sanskriti <sanskritisandhyagupta@gmail.com>
2023-07-16 11:10:35 +0530

    jump game

---
+++ Day 94/src/jumpGame.java
@@ -0,0 +1,21 @@
+
+public class jumpGame {
+	
+	 public boolean canJump(int[] nums) {
+	        if(nums.length <= 1){
+	            return true;
+	        }
+	        int stepsleft = nums[0];
+	        int index = 1;
+	        while(stepsleft > 0){
+	            if (index == nums.length-1){
+	                return true;
+	            }
+	            stepsleft = Math.max(stepsleft-1, nums[index]);
+	            index++;
+	        }
+	        return false;
+	    }
+	
+
+}
